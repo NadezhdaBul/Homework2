d8462b87cbf4fce524228e0ecc7fc952df77c72d
NadezhdaBul <Bulygina_n_a@mail.ru>
2023-02-22 21:30:48 +0300

    HW-3draft

--- src/test/java/Lesson2.java
+++ src/test/java/Lesson2.java
@@ -17,8 +17,51 @@ public class Lesson2 {
                 shouldHave(text("SoftAssertions")).click();
         $$(".markdown-body li").get(2).shouldHave(text("JUnit5 extension"));
         $(".markdown-body").shouldBe(text("Using JUnit5 extend test class"));
-
-
+        $(".markdown-body").shouldHave(text("@"))
+                .shouldHave(text("ExtendWith"))
+                .shouldHave(text("({"))
+                .shouldHave(text("SoftAssertsExtension"))
+                .shouldHave(text("."))
+                .shouldHave(text("class"))
+                .shouldHave(text("."))
+                .shouldHave(text("})"))
+                .shouldHave(text("class"))
+                .shouldHave(text("Tests"))
+                .shouldHave(text("{"))
+                .shouldHave(text("@"))
+                .shouldHave(text("void"))
+                .shouldHave(text("Test"))
+                .shouldHave(text("test"))
+                .shouldHave(text("() {"))
+                .shouldHave(text("Configuration"))
+                .shouldHave(text("."))
+                .shouldHave(text("assertionMode"))
+                .shouldHave(text("="))
+                .shouldHave(text("SOFT"))
+                .shouldHave(text(";"))
+                .shouldHave(text("open"))
+                .shouldHave(text("("))
+                .shouldHave(text("page.html"))
+                .shouldHave(text(")"))
+                .shouldHave(text("$"))
+                .shouldHave(text("#first"))
+                .shouldHave(text(")."))
+                .shouldHave(text("should"))
+                .shouldHave(text("("))
+                .shouldHave(text("visible"))
+                .shouldHave(text(")."))
+                .shouldHave(text("click"))
+                .shouldHave(text("();"))
+                .shouldHave(text("$"))
+                .shouldHave(text("("))
+                .shouldHave(text("#second"))
+                .shouldHave(text(")."))
+                .shouldHave(text("should"))
+                .shouldHave(text("("))
+                .shouldHave(text("visible"))
+                .shouldHave(text(")."))
+                .shouldHave(text("click"))
+                .shouldHave(text("(); } }"));
     }
-
 }
+
